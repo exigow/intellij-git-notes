f68cfe561fee891a5ed654c9bb02e3449c6b3cee
Jakub Blach <jakubblach@gmail.com>
2026-07-27 21:32:40 +0200

    fix note status toString (it's used in copy commit feature)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("org.jetbrains.intellij.platform")
 }
 
-version = "1.0.1"
+version = "1.0.2"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/notes/status/NotesStatus.kt
+++ src/main/kotlin/notes/status/NotesStatus.kt
@@ -2,9 +2,16 @@ package notes.status
 
 import com.intellij.vcs.log.CommitId
 import com.intellij.vcs.log.data.VcsCommitExternalStatus
+import notes.MessageBundle
 
 internal data class NotesStatus(val topics: List<String>, val commitId: CommitId? = null) : VcsCommitExternalStatus {
     companion object {
         val NONE = NotesStatus(emptyList())
+    }
+
+    override fun toString() = when (topics.size) {
+        0 -> ""
+        1 -> MessageBundle.message("notes.statusSingle")
+        else -> MessageBundle.message("notes.statusMultiple", topics.size)
     }
 }
